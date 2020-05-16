0274311931b0912ea21199275769fd62d5de7ab2
unknown <szhang@alumni.unca.edu>
2020-05-16 19:41:11 -0400

    Init

--- src/index.js
+++ src/index.js
@@ -1,35 +1,119 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheet/index.css';
-import UnitStatCalculator from './components/UnitStatCalculator.js'
-import LanguageChanger from './components/LanguageChanger.js';
 
-class Calculator extends React.Component {
-	constructor(props) {
-		super(props);
-		this.updateLanguage = this.updateLanguage.bind(this);
-		this.state = {
-			language: "en"
-		};
-	}
-	
-	updateLanguage(lang) {
-		this.setState({ language: lang });
-	}
+function Square(props) {
+	return (
+	  <button className="square" onClick={props.onClick}>
+		{props.value}
+	  </button>
+	);
+  }
   
-	render() {		
-		return(
-			<div class='calculator'>
-				<LanguageChanger onInput={this.updateLanguage} />
-				<UnitStatCalculator language={this.state.language} />			
+class Board extends React.Component {
+	renderSquare(i) {
+		return (
+		<Square
+			value={this.props.squares[i]}
+			onClick={() => this.props.onClick(i)}
+		/>
+		);
+	}
+
+	render() {
+		return (
+		<div>
+			<div className="board-row">
+				{this.renderSquare(0)}
+				{this.renderSquare(1)}
+				{this.renderSquare(2)}
+				{this.renderSquare(3)}
+				{this.renderSquare(4)}			
 			</div>
-		)
+			<div className="board-row">
+				{this.renderSquare(5)}
+				{this.renderSquare(6)}
+				{this.renderSquare(7)}
+				{this.renderSquare(8)}
+				{this.renderSquare(9)}			
+			</div>
+			<div className="board-row">
+				{this.renderSquare(10)}
+				{this.renderSquare(11)}
+				{this.renderSquare(12)}
+				{this.renderSquare(13)}
+				{this.renderSquare(14)}			
+			</div>
+			<div className="board-row">
+				{this.renderSquare(15)}
+				{this.renderSquare(16)}
+				{this.renderSquare(17)}
+				{this.renderSquare(18)}
+				{this.renderSquare(19)}			
+			</div>
+			<div className="board-row">
+				{this.renderSquare(20)}
+				{this.renderSquare(21)}
+				{this.renderSquare(22)}
+				{this.renderSquare(23)}
+				{this.renderSquare(24)}			
+			</div>						
+		</div>
+		);
 	}
 }
+  
+  class Game extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+		history: [
+		  {
+			squares: Array(25).fill(null)
+		  }
+		]
+	  };
+	}
 
-// ========================================
+	handleClick(i) {
+		console.log("AAAAAAAAAAA");
+	  }	
+  
+	render() {
+		const history = this.state.history;
+		const current = history[0];
 
-ReactDOM.render(
-  <Calculator />,
-  document.getElementById('root')
-);
+		return (
+			<div className="game">
+				<div className="game-board">
+				<Board squares={current.squares} onClick={i => this.handleClick(i)} />
+				</div>
+			</div>
+		);
+	}
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(<Game />, document.getElementById("root"));
+  
+  function calculateWinner(squares) {
+	const lines = [
+	  [0, 1, 2],
+	  [3, 4, 5],
+	  [6, 7, 8],
+	  [0, 3, 6],
+	  [1, 4, 7],
+	  [2, 5, 8],
+	  [0, 4, 8],
+	  [2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+	  const [a, b, c] = lines[i];
+	  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		return squares[a];
+	  }
+	}
+	return null;
+  }
+  
